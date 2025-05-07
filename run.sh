d608683716c7fa9a48d2aba1a13d008c2489fbc0
Al Rahrooh <al.rahrooh@outlook.com>
2025-05-07 12:44:07 -0400

    fix bash

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Evaluate different prompt types across same model 
-python pipeline.py \  --dataset datasets/cleaned/DrHCM_cleaned.csv \                                                           
+python pipeline.py \
+  --dataset datasets/cleaned/DrHCM_cleaned.csv \
   --output results/DrHCM_all_prompt_types_llama_3.2.csv \
   --prompt-models llama-3.2-1b \
   --answer-models llama-3.2-1b \
@@ -10,7 +11,8 @@ python pipeline.py \  --dataset datasets/cleaned/DrHCM_cleaned.csv \
 
 
 #Evaluate different models across same prompt types 
-python pipeline.py \  --dataset datasets/cleaned/medquad_cleaned.csv \                                                           
+python pipeline.py \
+  --dataset datasets/cleaned/medquad_cleaned.csv \
   --output results/medquad_self_consistency_5_models.csv \
   --prompt-models llama-3.2-1b deepseek-qwen-1.5b qwen3-1.7b gemma-3-1b-it granite-3.3-2b \
   --answer-models llama-3.2-1b deepseek-qwen-1.5b qwen3-1.7b gemma-3-1b-it granite-3.3-2b \
@@ -19,7 +21,8 @@ python pipeline.py \  --dataset datasets/cleaned/medquad_cleaned.csv \
   --num-questions 100 
 
 #Comprehensive evaluation of prompt types and models 
-python pipeline.py \  --dataset datasets/cleaned/pubmedqa_cleaned.csv \                                                           
+python pipeline.py \
+  --dataset datasets/cleaned/pubmedqa_cleaned.csv \
   --output results/pubmedqa_5_prompt_types_5_models.csv \
   --prompt-models llama-3.2-1b deepseek-qwen-1.5b qwen3-1.7b gemma-3-1b-it granite-3.3-2b \
   --answer-models llama-3.2-1b deepseek-qwen-1.5b qwen3-1.7b gemma-3-1b-it granite-3.3-2b \
